11b25f5c3850655cfc3440cc59c0db97434ad9af
ZEINASEIF <99324591+ZEINASEIF@users.noreply.github.com>
2022-03-31 15:41:40 +0200

    version1

---
+++ js/script.js
@@ -0,0 +1,37 @@
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+var form3 = document.getElementById("form3");
+var form4 = document.getElementById("form4");
+
+var next1 = document.getElementById("next1");
+var next2 = document.getElementById("next2");
+var previous = document.getElementById("previous");
+var submit = document.getElementById("submit");
+
+next1.onclick = function(){
+    form2.style.display = "block";
+    form1.style.display = "none";
+    form3.style.display = "none";
+    form4.style.display = "none";
+}
+
+previous.onclick = function(){
+    form2.style.display = "none";
+    form1.style.display = "block";
+    form3.style.display = "none";
+    form4.style.display = "none";
+}
+
+next2.onclick = function(){
+    form2.style.display = "none";
+    form1.style.display = "none";
+    form3.style.display = "block";
+    form4.style.display = "none";
+}
+
+submit.onclick = function(){
+    form2.style.display = "none";
+    form1.style.display = "none";
+    form3.style.display = "none";
+    form4.style.display = "block";
+}
